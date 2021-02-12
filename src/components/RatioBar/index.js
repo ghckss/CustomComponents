a1fa287f8dad27870eb90c679baec72ee65817bc
HwangHoChan <ghckss93@gmail.com>
2021-02-12 17:13:17 +0900

    router 설정

---
+++ src/components/RatioBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RatioBar = () => {
+    return (
+        <div>
+            <h2>
+                RatioBar
+            </h2>
+        </div>
+    );
+};
+
+export default RatioBar;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import RatioBar from "./RatioBar";
+
+export { Home, RatioBar };
