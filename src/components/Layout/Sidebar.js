21c1fbefd3aac728f13b579c0075a35281c6407d
HwangHoChan <ghckss93@gmail.com>
2021-02-24 23:46:09 +0900

    사이드바 ui 구성

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,7 +1,49 @@
 import React from "react";
+import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  const toggleSidebarItem = (e) => {
+    if (e.target.classList.contains("sidebar__item")) {
+      return;
+    }
+    const target = e.target.closest(".sidebar__dropdown");
+    target.classList.toggle("sidebar__dropdown--active");
+
+    changeIconDirection(target);
+    const sidebarItems = target.querySelectorAll(".sidebar__item");
+
+    Array.from(sidebarItems).forEach((item) => {
+      item.classList.toggle("sidebar__item--hide");
+    });
+  };
+  const changeIconDirection = (target) => {
+    const icon = target.querySelector("i.fas");
+    if (icon.classList.contains("fa-chevron-down")) {
+      icon.classList.remove("fa-chevron-down");
+      icon.classList.add("fa-chevron-right");
+    } else if (icon.classList.contains("fa-chevron-right")) {
+      icon.classList.remove("fa-chevron-right");
+      icon.classList.add("fa-chevron-down");
+    }
+  };
+  return (
+    <nav className="sidebar">
+      <div className="sidebar__dropdown" onClick={toggleSidebarItem}>
+        <div className="sidebar__name">
+          UI 컴포넌트
+          <i className="fas fa-chevron-right"></i>
+        </div>
+        <div className="sidebar__item sidebar__item--hide">비율 게이지</div>
+        <div className="sidebar__item sidebar__item--hide">계층형 테이블</div>
+      </div>
+      <div className="sidebar__dropdown" onClick={toggleSidebarItem}>
+        <div className="sidebar__name">
+          FE 스킬
+          <i className="fas fa-chevron-right"></i>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Sidebar;
