39ebcd16d7714e902311cb7707a840dafcf5ab62
HwangHoChan <ghckss93@gmail.com>
2021-02-27 18:50:14 +0900

    debounce, throttling url, sidebar 설정

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { ExpandableTableShowcase, RatioBarShowcase } from "../UIExample";
 import { Content, Header, Sidebar } from "../../components/Layout";
+import { Debounce, Throttling } from "../../components/Skills";
 
 const Layout = () => {
   return (
@@ -16,11 +17,15 @@ const Layout = () => {
             return <div className="home">HOME</div>;
           }}
         />
+
         <Route path="/widget/ratio-bar" render={RatioBarShowcase} />
         <Route
           path="/widget/table-expandable"
           render={ExpandableTableShowcase}
         />
+
+        <Route path="/skill/debounce" render={Debounce} />
+        <Route path="/skill/throttling" render={Throttling} />
       </Content>
     </div>
   );
