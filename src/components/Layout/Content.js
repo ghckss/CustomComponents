0792f6ec918c038435fd0c574e398a75f47aca16
HwangHoChan <ghckss93@gmail.com>
2021-02-27 18:42:17 +0900

    컨텐츠 컴포넌트 UI 조정 (#8)

--- src/components/Layout/Content.js
+++ src/components/Layout/Content.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Content.scss";
 
 const Content = ({ children }) => {
-  return <div className="content">{children}</div>;
+  return <main className="content">{children}</main>;
 };
 
 export default Content;
