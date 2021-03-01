50dd6ca09c9bdef4129eb6d3388964af238badf5
HwangHoChan <ghckss93@gmail.com>
2021-03-01 16:13:08 +0900

    debounce, throttling 페이지 업데이트 (#11)
    * 기능 함수로 분리
    
    * 입력시 API로 전달되는 검색어 리스트 출력
    
    * 레이아웃 변경 및 간략 설명 추가

--- src/components/Skills/Debounce.js
+++ src/components/Skills/Debounce.js
@@ -1,28 +1,60 @@
 import React from "react";
+import produce from "immer";
 import "./Debounce.scss";
 
 const Debounce = () => {
   const [timer, setTimer] = React.useState(null);
   const [inputValue, setInputValue] = React.useState("");
+
   const [activeCount, setActiveCount] = React.useState(0);
   const [deactiveCount, setDeactiveCount] = React.useState(0);
+
+  const [activeKeyword, setActiveKeyword] = React.useState([]);
+  const [deactiveKeyword, setDeactiveKeyword] = React.useState([]);
 
   const handleInputValue = (e) => {
     setInputValue(e.target.value);
     setDeactiveCount(deactiveCount + 1);
+    setDeactiveKeyword(
+      produce(deactiveKeyword, (draftState) => {
+        draftState.push(e.target.value);
+      })
+    );
+    debounce(e);
+  };
+
+  const debounce = (e) => {
     if (timer) {
       clearTimeout(timer);
       setTimer(timer);
     }
     setTimer(
-      setTimeout(function () {
+      setTimeout(() => {
         setActiveCount(activeCount + 1);
+        setActiveKeyword(
+          produce(activeKeyword, (draftState) => {
+            draftState.push(e.target.value);
+          })
+        );
       }, 200)
     );
   };
 
+  const renderDebouncingKeywords = (keywords) => {
+    return keywords.map((keyword, index) => {
+      return <li key={index}>{keyword}</li>;
+    });
+  };
+
   return (
     <div className="debouncing">
+      <h1>Debouncing</h1>
+      <div className="debouncing__description">
+        <h2>Debouncing 이란?</h2>
+        <span>자바스크립트의 이벤트를 성능상의 이유로 제어하기 위한 기법</span>
+        <br />
+        <span>이벤트를 그룹화하여 하나의 이벤트만 발생하도록 한다.</span>
+      </div>
       <div className="debouncing__title">
         <h2>Debouncing 비교 테스트</h2>
       </div>
@@ -36,12 +68,24 @@ const Debounce = () => {
       </div>
       <div className="result debouncing__result">
         <div className="result__container result__deactive">
-          <div className="result__title">Debounce 미 적용시 API 호출 횟수</div>
-          <div className="result__value">{deactiveCount}</div>
+          <div className="result__counter">
+            <div className="result__title">
+              Debounce 미 적용시 API 호출 횟수
+            </div>
+            <div className="result__value">{deactiveCount}</div>
+          </div>
+          <div className="result__list">
+            {renderDebouncingKeywords(deactiveKeyword)}
+          </div>
         </div>
         <div className="result__container result__active">
-          <div className="result__title">Debounce 적용시 API 호출 횟수</div>
-          <div className="result__value">{activeCount}</div>
+          <div className="result__counter">
+            <div className="result__title">Debounce 적용시 API 호출 횟수</div>
+            <div className="result__value">{activeCount}</div>
+          </div>
+          <div className="result__list">
+            {renderDebouncingKeywords(activeKeyword)}
+          </div>
         </div>
       </div>
     </div>
